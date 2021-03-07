474e3b5de8c6b097e86ff2b255e5fa3f8aced3ef
kelvinchukwueke26@gmail.com <www.kelvinchukwueke26@gmail.com>
2021-03-07 15:14:39 +0100

    new relase

---
+++ src/data/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  defaultTitle: 'chukwu kelvin',
+  logo: 'https://github.com/kelvin-c-web/ck-portfolio/favicon/favicon-512.png',
+  author: 'chukwu kelvin',
+  url: 'github.com/kelvin-c-web/ck-portfolio',
+  legalName: 'chukwu kelvin',
+  defaultDescription: 'I’m kelvin and I’m a full stack developer and an cyber security expert!',
+  socialLinks: {
+    github: 'https://github.com/kelvin-c-web/ck-portfolio',
+    stackoverflow: 'https://stackoverflow.com/users/11453767/kelvin-chukwu'
+  },
+  googleAnalyticsID: 'UA-88875900-4',
+  themeColor: '#6b63ff',
+  backgroundColor: '#6b63ff',
+  address: {
+    city: 'City',
+    region: 'Region',
+    country: 'Country',
+    zipCode: 'ZipCode',
+  },
+  contact: {
+    email: 'email',
+    phone: 'phone number',
+  },
+  foundingDate: '2021',
+};
